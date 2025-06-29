a8460ed4cefd4b626809ed8ca6a177a0f44e195f
Jose Mauro Mendes Junior <josemauromendesjunior@yahoo.com.br>
2025-06-29 20:59:42 -0300

    Aula 177

---
+++ GeekShopping.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,26 @@
+﻿using GeekShopping.CouponAPI.Data.ValueObjects;
+using GeekShopping.CouponAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private ICouponRepository _repository;
+
+        public CouponController(ICouponRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{couponCode}")]
+        public async Task<ActionResult<CouponVO>> GetCouoponByCouponCode(string couponCode)
+        {
+            var coupon = await _repository.GetCouoponByCouponCode(couponCode);
+            if(coupon == null) return NotFound();
+            return Ok(coupon);
+        }
+    }
+}
--- GeekShopping.CouponAPI/Program.cs
+++ GeekShopping.CouponAPI/Program.cs
@@ -1,11 +1,76 @@
+using AutoMapper;
+using GeekShopping.CouponAPI.Config;
+using GeekShopping.CouponAPI.Model.Context;
+using GeekShopping.CouponAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+
+IMapper mapper = MappingConfig.RegisterMapps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ICouponRepository, CouponRepository>();
+
+
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+{
+    options.Authority = "https://localhost:4435/";
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header
+                },
+                new List<string>()
+            }
+        });
+});
 
 var app = builder.Build();
 
@@ -17,7 +82,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
